cf56f2a9e6d4bd1a60a77e872662c5c1e017805a
Ладыгин Никита <olesya_lakunina@MacBook-Air-de-Olesa.local>
2026-04-14 21:36:57 +0300

    Add model field Flat.new_building.

--- property/models.py
+++ property/models.py
@@ -46,6 +46,11 @@ class Flat(models.Model):
         null=True,
         blank=True,
         db_index=True)
+    
+    new_building = models.BooleanField(
+        'новостройка',
+        null=True,
+        blank=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
