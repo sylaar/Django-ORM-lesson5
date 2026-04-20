a53e0cabf10906a9837439f5ebe20a659087a266
Ладыгин Никита <olesya_lakunina@MacBook-Air-de-Olesa.local>
2026-04-20 12:31:28 +0300

    Add TabularInline to Flat db.model.

--- property/admin.py
+++ property/admin.py
@@ -5,6 +5,10 @@ from .models import Flat
 from .models import Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Flat.owners.through
+    raw_id_fields = ['owner', 'flat',]
+
 class FlatAdmin(admin.ModelAdmin):
     raw_id_fields = ['liked_by']
     list_display = [
@@ -15,15 +19,17 @@ class FlatAdmin(admin.ModelAdmin):
         'town'
         ]
     list_editable = ['new_building']
-    list_filter = ['new_building', 'rooms_number']
+    list_filter = ['new_building', 'rooms_number',]
     search_fields = ('town', 'address', 'owners',)
     readonly_fields = ['created_at']
+    inlines = [OwnerInline]
 
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ['user', 'flat']
 
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ['owners_flats']
+
 
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Complaint, ComplaintAdmin)
