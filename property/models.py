97da998ba1d70151012ae424c648a6a23313b4f6
Ладыгин Никита <olesya_lakunina@MacBook-Air-de-Olesa.local>
2026-04-21 11:30:28 +0300

    Add db_index to new_building field in models.py

--- property/models.py
+++ property/models.py
@@ -9,50 +9,68 @@ class Flat(models.Model):
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
-        db_index=True)
-
-    description = models.TextField('Текст объявления', blank=True)
-    price = models.IntegerField('Цена квартиры', db_index=True)
-
+        db_index=True,
+        )
+    description = models.TextField(
+        'Текст объявления', 
+        blank=True,
+        )
+    price = models.IntegerField(
+        'Цена квартиры', 
+        db_index=True,
+        )
     town = models.CharField(
         'Город, где находится квартира',
         max_length=50,
-        db_index=True)
+        db_index=True,
+        )
     town_district = models.CharField(
         'Район города, где находится квартира',
         max_length=50,
         blank=True,
-        help_text='Чертаново Южное')
+        help_text='Чертаново Южное',
+        )
     address = models.TextField(
         'Адрес квартиры',
-        help_text='ул. Подольских курсантов д.5 кв.4')
+        help_text='ул. Подольских курсантов д.5 кв.4',
+        )
     floor = models.CharField(
         'Этаж',
         max_length=3,
-        help_text='Первый этаж, последний этаж, пятый этаж')
-
+        help_text='Первый этаж, последний этаж, пятый этаж',
+        )
     rooms_number = models.IntegerField(
         'Количество комнат в квартире',
-        db_index=True)
+        db_index=True,
+        )
     living_area = models.IntegerField(
         'количество жилых кв.метров',
         null=True,
         blank=True,
-        db_index=True)
-
-    has_balcony = models.BooleanField('Наличие балкона', null=True, blank=True, db_index=True)
-    active = models.BooleanField('Активно-ли объявление', db_index=True)
+        db_index=True,
+        )
+    has_balcony = models.BooleanField(
+        'Наличие балкона', 
+        null=True, 
+        blank=True, 
+        db_index=True,
+        )
+    active = models.BooleanField(
+        'Активно-ли объявление', 
+        db_index=True,
+        )
     construction_year = models.IntegerField(
         'Год постройки здания',
         null=True,
         blank=True,
-        db_index=True)
-    
+        db_index=True,
+        )
     new_building = models.BooleanField(
         'новостройка',
         null=True,
-        blank=True)
-    
+        blank=True,
+        db_index=True,
+        )
     liked_by = models.ManyToManyField(
         User,
         related_name='likes', 
@@ -81,6 +99,7 @@ class Complaint(models.Model):
     text = models.TextField(
         verbose_name='текст жалобы',
     )
+
     def __str__(self):
         return f'{self.text}'
     
